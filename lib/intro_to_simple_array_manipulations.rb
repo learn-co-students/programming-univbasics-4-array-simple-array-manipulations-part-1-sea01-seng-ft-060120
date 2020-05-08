485810728dbdc024a52cab3acbe33515385c07dd
Daniel Patnode <dbpatnode@hotmail.com>
2020-05-08 17:59:41 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,26 @@
+def using_push (array, string)
+  array= ["1","2","3","4","5","6"]
+  string = array.push("violet")
+end
+
+def using_unshift (array, string)
+  array = ["1", "2", "3", "4" ]
+  string = array.unshift("Staten Island")
+end
+
+def using_pop (array)
+  array.pop()
+end
+
+def pop_with_args (array)
+  array.pop(2)
+end
+
+def using_shift (array)
+  array.shift()
+end
+
+def shift_with_args (array)
+  array.shift(2)
+end
+
